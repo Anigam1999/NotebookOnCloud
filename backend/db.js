ef24671c35f166fe14e8743663ff67ad1e0c96bc
Abhishek Nigam <abhisheknigam755@gmail.com>
2023-08-21 23:33:46 +0530

    add mongoose connection

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const mongoURI = 'mongodb://127.0.0.1:27017/'
+
+const connectToMongo = async ()=>{
+    try{
+        await mongoose.connect(mongoURI)
+        console.log('connected to mongoDB')
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+
+module.exports = connectToMongo;
